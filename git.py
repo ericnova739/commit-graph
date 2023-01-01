c2ee55ce1dcae66f857c52f2d69fdba60c8352ce
ericnova738 <ui033414@gmail.com>
2023-01-01 12:00:00 +0500

    commit on 2023-01-01

--- git.py
+++ git.py
@@ -1,13 +1,22 @@
 import os
+from datetime import datetime, timedelta
+import random
 
-with open("test.txt", "r") as f:
-    dates = f.readlines()
+start_date = datetime(2023, 1, 1)   # change this
+end_date = datetime(2026, 4, 10)    # today
 
-for date in dates:
-    date = date.strip()
+current = start_date
 
-    with open("file.txt", "a") as file:
-        file.write(date + "\n")
+while current <= end_date:
+    commits_per_day = random.randint(1, 5)  # 1–5 commits per day
 
-    os.system("git add .")
-    os.system(f'GIT_AUTHOR_DATE="{date}T12:00:00" GIT_COMMITTER_DATE="{date}T12:00:00" git commit -m "commit on {date}"')
+    for _ in range(commits_per_day):
+        date_str = current.strftime("%Y-%m-%dT12:00:00")
+
+        with open("file.txt", "a") as f:
+            f.write(date_str + "\n")
+
+        os.system("git add .")
+        os.system(f'GIT_AUTHOR_DATE="{date_str}" GIT_COMMITTER_DATE="{date_str}" git commit -m "commit on {current.strftime("%Y-%m-%d")}"')
+
+    current += timedelta(days=1)
